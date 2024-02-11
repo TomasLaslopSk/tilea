6b3fa85e05b3c602983be2ffa93019fef6d9f3eb
TomasLaslopSK <lasloptomas@gmail.com>
2024-02-11 22:39:17 +0100

    Actual version live

---
+++ index.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kaderníctvo Tilea Hair</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preload" href="workplace1.jpg" as="image">
+    <link rel="preload" href="workplace2.jpg" as="image">
+    <link rel="preload" href="workplace3.jpg" as="image">
+    <link rel="preload" href="workplace4.jpg" as="image">
+    <link rel="preload" href="workplace5.jpg" as="image">
+    <link rel="preload" href="about.png" as="image">
+    <link rel="preload" href="balayage.jpeg" as="image">
+    <link rel="preload" href="dark_hair_1.png" as="image">
+    <link rel="preload" href="dark_hair_2.png" as="image">
+    <link rel="preload" href="blond.png" as="image">
+    <link rel="preload" href="man_cut.jpeg" as="image">
+    <link rel="preload" href="kid_cut.jpeg" as="image">
+    <link rel="preload" href="ombre.png" as="image">
+    <link rel="preload" href="red-hair.jpeg" as="image">
+    <link rel="preload" href="services-2.jpeg" as="image">
+
+
+</head>
+<body>
+    <!-- header -->
+    <section class="header">
+
+        <a href="#" class="logo"> 
+            <div class="icons">
+                <img src="images/main-logo-transparent.png" alt="">
+             </div>   
+        </a>
+     
+        <nav class="navbar">
+           <div id="close-navbar" class="fas fa-times"></div>
+           <a href="#home">domov</a>
+           <a href="#services">služby</a>
+           <a href="#styles">kozmetika</a>
+           <a href="#pricing">cenník</a>
+           <a href="#about">o nás</a>
+        </nav>
+     
+        <div id="menu-btn" class="fas fa-bars"></div>
+     
+     </section>
+<!-- header -->
+
+<!-- home -->
+
+<section class="home" id="home">
+
+    <div class="row">
+        <div class="content">
+            <h3> Salón <br> ktorý ponúka <br> unikátny zážitok </h3>
+            <span>V historickom <br> centre Žiliny</span>
+            <div class="appointment_btn_wrapper">
+            <a href="tel:+421911058948" class="link"> 
+                <button class="btn"> Chcem termín</button>
+            </a>
+        </div>
+        </div>
+
+        <div class="image-slider" id="slider">
+            <div class="arrow-left">
+                <i class="fas fa-arrow-left"></i>
+            </div>
+            <!-- Todo: delete this and try to style in CSS if this is commented slider is down -->
+            <div class="image">
+                <img src="images/workplace1.jpg" alt="">
+            </div>
+            <div class="arrow-right">
+                <i class="fas fa-arrow-right"></i>
+            </div>
+        <div>
+    </div>
+
+</section>
+<!-- home -->
+
+<!-- services -->
+
+<section class="services" id="services">
+
+    <h1 class="heading">Služby</h1>
+
+    <section class="services_prologue">
+
+        <div class="row">
+
+            <!-- <div class="image">
+                <img src="images/about.png" alt="">
+            </div> -->
+
+            <div class="content">
+                <div class="icons-container">
+                    <div class="icons">
+                    <img src="images/about-icon-1.png" alt="">
+                    <h3>Pánske, dámske a detské kaderníctvo</h3>
+                    </div>
+                    <div class="icons">
+                    <img src="images/about-icon-2.png" alt="">
+                    <h3>Predaj profesionálnej kozmetiky koncernu Loreal Paris</h3>
+                    </div>
+                    <div class="icons">
+                    <img src="images/about-icon-3.png" alt="">
+                    <h3>Regenerácia vlasov</h3>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <section class="work_examples">
+            <div class="row">
+                <div class="content">
+                    <h3 class="title">Ukážka mojich prác</h3>
+                </div>
+            </div>
+    </section>
+
+    <div class="insta_btn_wrapper">
+        <a href="https://www.instagram.com/tilea_hair/?igsh=MXg0c2tidHZsdzhpeg%3D%3D" class="link"> 
+            <button class="btn"> Instagram</button>
+        </a>
+    </div>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="images/dark_hair_2.png" alt="">
+            <div class="content">
+                <h3>Farbenie</h3>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/blond.png" alt="">
+            <div class="content">
+                <h3>Ombre</h3>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/man_cut.jpeg" alt="">
+            <div class="content">
+                <h3>Pánsky strih</h3>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/kid_cut.jpeg" alt="">
+            <div class="content">
+                <h3>Detské farbenie</h3>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/red-hair.jpeg" alt="">
+            <div class="content">
+                <h3>Červené ombré</h3>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/about.png" alt="">
+            <div class="content">
+                <h3>Spoločenský účes</h3>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/services-2.jpg" alt="">
+            <div class="content">
+                <h3>Svadobné účesy</h3>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/balayage.jpeg" alt="">
+            <div class="content">
+                <h3>Melír</h3>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="images/ombre.jpeg" alt="">
+            <div class="content">
+                <h3>Ombre</h3>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/dark_hair.png" alt="">
+            <div class="content">
+                <h3>Farbenie</h3>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<!-- services end -->
+
+<!-- styles -->
+
+<section class="styles" id="styles">
+
+    <h1 class="heading">Kozmetika</h1>
+
+    <div class="box-container">
+    </div>
+
+    <div class="brands">
+         <img src="images/redken.png" alt="">
+    <div>
+
+    <div class="brands">
+        <img src="images/matrix.png" alt="">
+    <div>
+
+</section>
+
+<!-- styles end-->
+
+
+<!-- pricing -->
+
+<section class="pricing" id="pricing">
+
+    <h1 class="heading">Cenník</h1>
+
+    <div class="box-container">
+        <div class="box">
+            <div class="image">
+                <img src="images/pricing_1.jpeg" alt="">
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="images/pricing_2.jpeg" alt="">
+            </div>
+        </div>
+        <div class="box">
+            <div class="image">
+                <img src="images/pricing_3.jpeg" alt="">
+            </div>
+        </div>
+    </div>
+     
+</section>
+
+<!-- pricing end -->
+
+<!-- about us -->
+
+<section class="about" id="about">
+
+    <h1 class="heading">o nás</h1>
+
+    <div class="row">
+
+        <div class="content">
+            <!-- <h3 class="title">Som vlasová stylistka</h3> -->
+                <span> Pracujem v obore 10 rokov</span>        
+                <span> Ponúkam individuálny prístup, učesy na mieru</span>
+                <span> Špecializujem sa na farbenie vlasov, melír, balayage</span>
+                <span> Predlžovanie vlasov</span>
+                <span> Môj salón  ponúka detský kútik</span>
+                <span> Platba kartou je u nás možná</span>
+                <span> Pre kávičkárov máme k dispozíci automatický kávovar</span>
+                <span> Vnútorná oddychová zóna s TV a aj vonkajšou terasou</span>
+        </div>
+    </div>
+
+</section>
+
+<!-- about us end -->
+
+<!-- footer -->
+
+<section class="footer">
+
+    <div class="box-container">
+ 
+       <div class="box">
+          <h3> Sledujte nás </h3>
+          <div class="share">
+             <a href="https://www.facebook.com/profile.php?id=100040720166739" class="fab fa-facebook-f"></a>
+             <a href="https://www.instagram.com/tilea_hair/?igsh=MXg0c2tidHZsdzhpeg%3D%3D" class="fab fa-instagram"></a>
+          </div>
+       </div>
+ 
+       <div class="box">
+          <h3>kontaktujte nás</h3>
+          <a href="tel:+421911058948" class="link"> +421911058948</a>
+          <a href="#" class="link">krcmarik.janka@gmail.com</a>
+       </div>
+ 
+       <div class="box">
+          <h3>lokalita</h3>
+          <p class="link">Jozefa Vuruma 7 <br>
+            Žilina <br></p>
+       </div>
+ 
+    </div>
+ 
+    <div class="credit"> created by <span>testpipe s.r.o.</span> | všetky práva vyhradené! </div>
+ 
+ </section>
+
+<!-- footer end-->
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
--- js/script.js
+++ js/script.js
@@ -1,15 +1,89 @@
+
+// Constants and variables 
+
+// Slider picture
+let id = 0
+
+// Elements
 let navbar = document.querySelector('.header .navbar');
 let menuBtn = document.querySelector('#menu-btn');
 let closeBtn = document.querySelector('#close-navbar');
 
-menuBtn.onclick = () =>{
+// Slider pictures array 
+const images = [
+   'workplace1.jpg', 'workplace2.jpg', 'workplace3.jpg', 'workplace4.jpg', 'workplace5.jpg'
+]
+
+// Functions 
+
+menuBtn.onclick = () => {
    navbar.classList.add('active');
 };
 
-closeBtn.onclick = () =>{
+closeBtn.onclick = () => {
     navbar.classList.remove('active');
  };
 
-window.onscroll = () =>{
+window.onscroll = () => {
    navbar.classList.remove('active');
 };
+
+// Slider arrows click
+document.querySelector('.arrow-left').addEventListener('click', () => {
+   id--;
+   if(id < 0) {
+      id = images.length - 1;
+   }
+   slide(id);
+});
+
+document.querySelector('.arrow-right').addEventListener('click', () => {
+   id++;
+   if(id > images.length - 1) {
+      id = 0;
+   }
+   slide(id);
+});
+
+
+// Sliding function 
+function slide(id) {
+   let slider = document.querySelector('#slider')
+   slider.style.backgroundImage = `url(../images/${images[id]})`;
+   // fadeOut(slider, 500)
+   fadeIn(slider)
+}
+
+function autoSliding() {
+   deleteInterval = setInterval(timer, 30000);
+   function timer() {
+      if (id < images.length - 1) {
+         id++;
+      } else {
+         id = 0
+      }
+      slide(id)
+   }
+}
+
+// Slider fade effect
+function fadeIn(element) {
+   (function increment(value = 0) {
+       element.style.opacity = String(value);
+
+      if (element.style.opacity == '0') {
+         setTimeout(() => {
+             increment(value + 0.1);
+         }, 200);
+      }
+
+      if (element.style.opacity >= '0.1' && element.style.opacity !== '1') {
+           setTimeout(() => {
+               increment(value + 0.1);
+         }, 100);
+      }
+   })();
+};
+
+// Runtime
+autoSliding();
